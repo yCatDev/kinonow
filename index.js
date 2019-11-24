6125b102ddbf8f5f02ac3d0a322ef8840b84b16b
Grebenuk Denis <32875166+yCatDev@users.noreply.github.com>
2019-11-24 22:22:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-let http = require('http');
-let fs = require('fs');
- 
-let handleRequest = (request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    fs.readFile('./index.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            respone.write('Whoops! File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-};
- 
-http.createServer(handleRequest).listen(8000);
+var http = require('http');
+
+http.createServer(function (request, response) {
+
+  response.writeHead(200, {
+     'Content-Type': 'text/plain'
+  });
+
+  console.log('Example app listening on port 8080!')
+  response.end('Hello HTTP!');
+  console.log('Example app listening on port 8080!')
+}).listen(8080);
